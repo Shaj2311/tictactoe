24869e683089fc4605758d9fbfda8e0cee257141
Shaj2311 <haideralijafri2007@gmail.com>
2026-01-15 16:59:32 +0500

    Enable colors and initialize color pairs

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,13 @@ int main(int argc, char **argv) {
   initscr();
   cbreak();
 
+  /* Initialize colors */
+  start_color();
+  use_default_colors();
+  init_pair(0, COLOR_WHITE, -1);
+  init_pair(1, COLOR_RED, -1);
+  init_pair(2, COLOR_GREEN, -1);
+
   while (1) {
     vector<int> startingState = {-1, -1, -1, -1, -1, -1, -1, -1, -1};
     Game game = Game(startingState);
